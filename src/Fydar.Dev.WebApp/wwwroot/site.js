2938173c67bdd84774dd5dcf578de1879a37c498
fydar <fydar.dev@gmail.com>
2026-06-21 23:32:10 +0100

    🐛 Fix transition issue

--- src/Fydar.Dev.WebApp/wwwroot/site.js
+++ src/Fydar.Dev.WebApp/wwwroot/site.js
@@ -2,21 +2,6 @@
 queueMicrotask(console.log.bind(console, "%c   ___             __\n /\'___\\           /\\ \\\n/\\ \\__/  __  __   \\_\\ \\     __     _ __\n\\ \\ ,__\\/\\ \\/\\ \\  /\'_` \\  /\'__`\\  /\\`\'__\\\n \\ \\ \\_/\\ \\ \\_\\ \\/\\ \\L\\ \\/\\ \\L\\.\\_\\ \\ \\/\n  \\ \\_\\  \\/`____ \\ \\___,_\\ \\__/.\\_\\\\ \\_\\\n   \\/_/   `/___/> \\/__,_ /\\/__/\\/_/ \\/_/\n             /\\___/\n             \\/__/", "font-family: monospace; white-space: nowrap"));
 queueMicrotask(console.log.bind(console, "This website has been made using ASP.NET Core and Blazor.\nLike what you see? I\'m available for hire!\n\nhttps://fydar.dev/contact"));
 
-const suppressTransitionAbort = (err) => { if (err.name !== 'AbortError') throw err; };
-
-document.addEventListener('pageswap', (e) => {
-    if (e.viewTransition) {
-        e.viewTransition.ready.catch(suppressTransitionAbort);
-        e.viewTransition.finished.catch(suppressTransitionAbort);
-    }
-});
-
-document.addEventListener('pagereveal', (e) => {
-    if (e.viewTransition) {
-        e.viewTransition.ready.catch(suppressTransitionAbort);
-        e.viewTransition.finished.catch(suppressTransitionAbort);
-    }
-});
 
 const clamp = (a, min = 0, max = 1) => Math.min(max, Math.max(min, a));
 const invlerp = (x, y, a) => (a - x) / (y - x);
